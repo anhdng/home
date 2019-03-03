533fe4f55f57b6216fd180096f4a90b1fc8701af
anhdng <ng4.anh@gmail.com>
2019-03-03 22:46:55 +0700

    updated site

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 $('.view').magnificPopup({
-  type: 'image'
-  // other options
+		type: 'image',
+		closeOnContentClick: true,
+		closeBtnInside: false,
+		fixedContentPos: true,
+		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		image: {
+			verticalFit: true
+		},
+		zoom: {
+			enabled: true,
+			duration: 300 // don't foget to change the duration also in CSS
+		}
 });
