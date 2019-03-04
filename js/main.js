a8b10dd42053f7bc69b9fa4ae4efabb87ca9d32b
anhdng <ng4.anh@gmail.com>
2019-03-04 17:26:09 +0700

    updated site

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ $(document).ready(function() {
   // $('.view').magnificPopup({
   //   type:'image'
   // });
-  $('.view').magnificPopup({
+  $('.image-popup-no-margins').magnificPopup({
 		type: 'image',
 		closeOnContentClick: true,
 		closeBtnInside: false,
 		fixedContentPos: true,
-		mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
+		mainClass: 'mfp-no-margins', // class to remove default margin from left and right side
 		image: {
 			verticalFit: true
 		},
